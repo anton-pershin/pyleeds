a84b92395211d13831236837d55a0b38cfad15b2
Anton Pershin <tony.pershin@gmail.com>
2017-03-21 13:22:50 +0000

    Added tools for parsing data files of different types.

--- postproc/auxtools.py
+++ postproc/auxtools.py
@@ -1,4 +1,5 @@
 import numpy as np
+import inspect
 
 class NamedAttributesContainer(object):
     def __init__(self, elements, elements_names):
@@ -58,18 +59,68 @@ class LabeledValue(object):
     def __str__(self):
         return str(self.val)
 
+#class LabeledList(object):
+#    def __init__(self):
+#        self.values = []
+#        self.label = None
+#
+#    def append(self, labeled_value):
+#        if self.label is None:
+#            self.label = labeled_value.label
+#        elif self.label != labeled_value.label:
+#            raise LabelsDoNotMatch('List label and appended value label do not match')
+#
+#        self.values.append(labeled_value.val)
+
 class LabeledList(object):
-    def __init__(self):
-        self.values = []
-        self.label = None
-
-    def append(self, labeled_value):
-        if self.label is None:
-            self.label = labeled_value.label
-        elif self.label != labeled_value.label:
-            raise LabelsDoNotMatch('List label and appended value label do not match')
-
-        self.values.append(labeled_value.val)
+    def __init__(self, values, label):
+        self.values = values
+        self.label = label
 
 class LabelsDoNotMatch(Exception):
     pass
+
+def is_sequence(arg):
+    return (not hasattr(arg, "strip") and
+            hasattr(arg, "__getitem__") or
+            hasattr(arg, "__iter__"))
+
+def parse_datafile(path, data_names, transform_funcs, cols_to_parse=[]):
+    if cols_to_parse == []:
+        cols_to_parse = range(len(data_names))
+    if len(data_names) != len(transform_funcs) or len(data_names) != len(cols_to_parse):
+        raise Exception('Number of data names, transform functions and columns to be parsed is inconsistent')
+    data = {}
+    for data_name in data_names:
+        data[data_name] = []
+
+    f = open(path, 'r') # if not found, expection will be raised anyway
+    lines = f.readlines()
+    for line in lines[1:]: # skip the first line
+        tmp = line.split()
+        if len(tmp) < len(data_names):
+            raise Exception('Number of given data names is larger than number of columns we have in the data file.')
+        for i, data_name in enumerate(data_names):
+            val = tmp[cols_to_parse[i]]
+            data[data_name].append(transform_funcs[i](val))
+    return data
+
+def parse_timed_numdatafile(path):
+    time = []
+    data = []
+    f = open(path, 'r') # if not found, expection will be raised anyway
+    lines = f.readlines()
+    for line in lines[1:]: # skip the first line
+        tmp = line.split()
+        time.append(float(tmp[0]))
+        timed_data = np.zeros((len(tmp) - 1, ))
+        for i, val in enumerate(tmp[1:]):
+            timed_data[i] = float(val)
+        data.append(timed_data)
+    return time, data
+
+def np_index(np_array, val):
+    '''
+    Returns index corresponding to the nearest element
+    '''
+    return np.abs(np_array - val).argmin()
